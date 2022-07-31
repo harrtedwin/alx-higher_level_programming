3bd628f871389bdd747a5e007f95ab2eb3b040a1
harrtedwin <mkihara.edwin@gmail.com>
2022-07-31 16:57:57 +0300

    minor changes

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-from add_0 import add
 
 if __name__ == "__main__":
-    """
+    """Print the sum of 1 and 2."""
+    from add_0 import add
 
-    Prints the result of the addition between two numbers
-
-    """
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-import sys
 
-if __name__ == '__main__':
-    av = sys.argv
-    l_av = len(av)
-    sum = 0
+if __name__ == "__main__":
+    """Print the addition of all arguments."""
+    import sys
 
-    if l_av > 1:
-        for i in range(1, l_av):
-            sum += int(av[i])
-
-    print(sum)
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
